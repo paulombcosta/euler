f7e17bae49f37b5687fb9ee95d917a0c2a6b1ed4
Paulo Costa <paulombcosta@gmail.com>
2016-02-26 04:20:38 -0300

    Making elm problems work again

--- elm/problem001.elm
+++ elm/problem001.elm
@@ -1,11 +1,14 @@
-import Graphics.Element exposing (..)
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
 import List exposing (..)
+import String exposing (..)
+import Html exposing (text)
 
-    -- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+main = sum
+     |> toString
+     |> text
 
-    -- Find the sum of all the multiples of 3 or 5 below 1000. 
-
-main = Graphics.Element.show sum
-
-sum : number
-sum = List.sum (filter (\x -> x % 3 == 0 || x % 5 == 0) [1..999])
+sum : Int
+sum = List.sum (List.filter (\x -> x % 3 == 0 || x % 5 == 0) [1..999])
