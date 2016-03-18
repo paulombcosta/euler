54d941d52d4d57ceafc4d192e0db102803fa515c
Paulo Costa <paulombcosta@gmail.com>
2016-03-18 17:29:51 -0300

    WIP

--- elm/Problem004.elm
+++ elm/Problem004.elm
@@ -16,3 +16,25 @@ highestPalindromeOf2 = 0
 
 highestPalindromeOf3 : Int
 highestPalindromeOf3 = 0
+
+genCombinationOf2 : List(Int, Int)
+genCombinationOf2 =
+  let
+    acc : Int -> List(Int, Int) -> List(Int, Int)
+    acc x xs =
+      if x > 99 then xs
+      else
+        acc (x + 1) xs ++ (List.map ((,) x) [10..99])
+   in
+     acc 9 []
+
+-- andMap : List (a -> b) -> List a -> List b -- like (<*>) in Haskell, specialised to lists
+-- andMap listOfFuncs listOfAs = List.concatMap (\f -> List.map f listOfAs) listOfFuncs
+
+-- gridOf12x24 = List.map (,) [1..12] `andMap` [1..24]
+
+compute : Int
+compute =
+  case (genCombinationOf2 |> List.map (\(x,y) -> x * y) |> List.filter isPalindrome |> List.maximum) of
+    Just a -> a
+    Nothing -> 0
